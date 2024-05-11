2ec30667cc81c2c8c8e925512fcb8fb76e5e37a9
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-05-11 15:14:01 +0330

    register part added

--- Codes/User/user.py
+++ Codes/User/user.py
@@ -5,6 +5,7 @@ import globals
 from rich import print
 
 class Account:
+    
     def __init__(self, username = None , password = None, email_address = None):
         self.__username = username
         self.__password = password
