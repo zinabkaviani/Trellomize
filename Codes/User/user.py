45a542c022ca4469c7a6e2b19bb60144861f341f
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-05-11 16:58:26 +0330

    update users

--- Codes/User/user.py
+++ Codes/User/user.py
@@ -39,51 +39,51 @@ class Account:
                 setattr(self, key, value)
 
 
-def register():
-    username = input("Enter username: ")
-    email = input("Enter email address: ")
-    password = input("Enter password: ")
+    def register(self):
+        self.__username = input("Enter username: ")
+        self.__email_address = input("Enter email address: ")
+        self.__password = input("Enter password: ")
 
-    if check_existing(username, email):
-        print("[bold red]Username or email already exists.[/bold red]")
-    else:
-        with open("Data\\Acounts_Data\\users.txt", "a") as file:
-            file.write(f"{username},{email}\n")
-        print("[bold green]Account successfully created.[/bold green]")
+        if self.check_existing(self.__username, self.__email_address):
+            print("[bold red]Username or email already exists.[/bold red]")
+        else:
+            with open("Data\\Acounts_Data\\users.txt", "a") as file:
+                file.write(f"{self.__username},{self.__email}\n")
+            print("[bold green]Account successfully created.[/bold green]")
 
-def check_existing(username, email):
-    if os.path.exists("Data\\Acounts_Data\\users.txt"):
-        with open("Data\\Acounts_Data\\users.txt", "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                stored_username, stored_email = line.strip().split(',')
-                if username == stored_username or email == stored_email:
-                    return True
-    return False
+    def check_existing(self):
+        if os.path.exists("Data\\Acounts_Data\\users.txt"):
+            with open("Data\\Acounts_Data\\users.txt", "r") as file:
+                lines = file.readlines()
+                for line in lines:
+                    stored_username, stored_email = line.strip().split(',')
+                    if self.__username == stored_username or self.__email_address == stored_email:
+                        return True
+        return False
 
-def change_email(username):
-    new_email = input("Enter the new email address: ")
-    if not check_existing(username, new_email):
-        with open("Data\\Acounts_Data\\users.txt", "r") as file:
-            lines = file.readlines()
-        with open("Data\\Acounts_Data\\users.txt", "w") as file:
-            for line in lines:
-                stored_username, stored_email = line.strip().split(',')
-                if stored_username == username:
-                    file.write(f"{username},{new_email}\n")
-                else:
-                    file.write(line)
-        print("[bold green]Email address updated successfully.[/bold green]")
-    else:
-        print("[bold red]Email address already exists.[/bold red]")
+    def change_email(self):
+        new_email = input("Enter the new email address: ")
+        if not self.check_existing(self.__username, new_email):
+            with open("Data\\Acounts_Data\\users.txt", "r") as file:
+                lines = file.readlines()
+            with open("Data\\Acounts_Data\\users.txt", "w") as file:
+                for line in lines:
+                    stored_username, stored_email = line.strip().split(',')
+                    if stored_username == self.__username:
+                        file.write(f"{self.__username},{new_email}\n")
+                    else:
+                        file.write(line)
+            print("[bold green]Email address updated successfully.[/bold green]")
+        else:
+            print("[bold red]Email address already exists.[/bold red]")
 
-def register_section():
-    options = ["Register", "Change Email"]
-    choice = globals.get_arrow_key_input(options)
+    def register_section(self):
+        options = ["Register", "Change Email"]
+        choice = globals.get_arrow_key_input(options)
 
-    if choice == "Register":
-        register()
-    elif choice == "Change Email":
-        username = input("Enter your username: ")
-        change_email(username)
+        if choice == "Register":
+            self.register()
+        elif choice == "Change Email":
+            username = input("Enter your username: ")
+            self.change_email(username)
 
