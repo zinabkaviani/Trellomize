f098bed7f4dc96a11432c8b5b28cee997d30df08
amir-rahmani84 <152492156+amir-rahmani84@users.noreply.github.com>
2024-05-11 14:43:32 +0330

    Update user.py

---
+++ Codes/User/user.py
@@ -0,0 +1,35 @@
+import bcrypt
+import json
+
+class Account:
+    def __init__(self, username = None , password = None, email_address = None):
+        self.__username = username
+        self.__password = password
+        self.__email_address = email_address
+
+    def get_username(self):
+        return self.__username
+
+    def get_email_address(self):
+        return self.__email_address
+
+    def encode_password(password_input):
+        return bcrypt.hashpw(password_input.encode('utf-8'), bcrypt.gensalt())
+    
+    def check_password(entered_password , hashed_password):
+        return bcrypt.checkpw(entered_password.encode('utf-8'), hashed_password)
+    
+    def save_user_data(self):
+        user_data = {
+            "username": self.__username,
+            "password_hash": self.encode_password(self.__password),  # Save the password hash directly as bytes
+            "email": self.__email_address
+        }
+        with open('Data\\Acount_Data\\user_data.json', 'w') as file:
+            json.dump(user_data, file)
+
+    def set_attributes_from_json(self, filename):
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            for key , value in data:
+                setattr(self, key, value)
