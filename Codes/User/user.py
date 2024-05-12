f686668ec29672853e80973b5d343184c80a5a80
amir-rahmani84 <152492156+amir-rahmani84@users.noreply.github.com>
2024-05-13 00:32:42 +0330

    User class base added

--- Codes/User/user.py
+++ Codes/User/user.py
@@ -1,15 +1,16 @@
 import bcrypt
 import json
 import os
-import globals
+from .. import globals
 from rich import print
 
 
 class Account:
     
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, username="", email_address="" , password = ""):
+        self.__username = username
+        self.__email_address = email_address
+        self.__password = password
     
     def get_username(self):
         return self.__username
@@ -46,13 +47,13 @@ class Account:
         if self.check_existing(self.__username, self.__email_address):
             print("[bold red]Username or email already exists.[/bold red]")
         else:
-            with open("Data\\Acounts_Data\\users.txt", "a") as file:
+            with open("Data\\Accounts_Data\\users.txt", "a") as file:
                 file.write(f"{self.__username},{self.__email}\n")
             print("[bold green]Account successfully created.[/bold green]")
 
     def check_existing(self):
-        if os.path.exists("Data\\Acounts_Data\\users.txt"):
-            with open("Data\\Acounts_Data\\users.txt", "r") as file:
+        if os.path.exists("Data\\Accounts_Data\\users.txt"):
+            with open("Data\\Accounts_Data\\users.txt", "r") as file:
                 lines = file.readlines()
                 for line in lines:
                     stored_username, stored_email = line.strip().split(',')
@@ -63,9 +64,9 @@ class Account:
     def change_email(self):
         new_email = input("Enter the new email address: ")
         if not self.check_existing(self.__username, new_email):
-            with open("Data\\Acounts_Data\\users.txt", "r") as file:
+            with open("Data\\Accounts_Data\\users.txt", "r") as file:
                 lines = file.readlines()
-            with open("Data\\Acounts_Data\\users.txt", "w") as file:
+            with open("Data\\Accounts_Data\\users.txt", "w") as file:
                 for line in lines:
                     stored_username, stored_email = line.strip().split(',')
                     if stored_username == self.__username:
@@ -83,9 +84,53 @@ class Account:
         if choice == "Register":
             self.register()
         elif choice == "Change Email":
-            username = input("Enter your username: ")
-            self.change_email(username)
+            self.change_email()
 
-x = Account()
-x.__username = 1
-print(x.get_username())
+class User:
+    def __init__(self, account, leading_projects, contributing_projects, is_active = True):
+        self.__account = account
+        self.__leading_projects = leading_projects
+        self.__contributing_projects = contributing_projects
+        self.__is_active = is_active
+    
+    def display_projects(self):
+        #opens the files of both types of projects the user has and then show the details somehow
+        #some features can be added such as choosing a project and showing more details about it without
+        #going into the project (making an object)
+        pass
+    
+    def choose_project(self):
+        while(True):
+            choice = globals.get_arrow_key_input(['Leading projects' , 'Contributing projects' , 'Exit'])
+            if choice == 'Leading projects':
+                result = self.choose_leading_projects()
+                if(result != None):
+                    return result
+            elif choice == 'Contributing projects':
+                result = self.choose_contributing_projects()
+                if(result != None):
+                    return result
+            else:
+                return
+    def choose_leading_projects(self):
+        Options = {}
+        for project in self.__leading_projects:
+            with open(f'Data\\Projects_Data\\{project}.json' , 'w') as file:
+                Data = json.load(file)
+                for key , value in Data:
+                    Options[project : value]
+        #show Options to choose to be added
+
+    def choose_contributing_projects(self):
+        Options = {}
+        for project in self.__contributing_projects:
+            with open(f'Data\\Projects_Data\\{project}.json' , 'w') as file:
+                Data = json.load(file)
+                for key , value in Data:
+                    Options[project : value]
+        #show Options to choose to be added
+
+    def create_project(self):
+        #needs a menu and checking if a project with the same id has been made before
+        #and then make a file and other stuff
+        pass
