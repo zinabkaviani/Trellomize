bed60437c5621cfaf27edae28404e2e1cf616790
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-05-15 13:10:52 +0330

    user menu added

--- Codes/User/user.py
+++ Codes/User/user.py
@@ -32,68 +32,80 @@ class Account:
         }
         with open(f'Data\\Acounts_Data\\Acounts\\{self.__username}.json', 'w') as file:
             json.dump(account_data, file)
+    
+    def account_setting_menu(self) :
+        option =["Change Email" , "Delete User" , "Sing Out", "Back"]
+        while True :
+            choice = globals.get_arrow_key_input(option)
+            match choice :
+                case "Change Email" :
+                    self.change_email(self)
+                case "Delete User" :
+                    pass
+                case "Sing Out" :
+                    return "sing out"
+                case "Back" :
+                    return "back"
 
-    def set_attributes_from_json(self, filename):
-        self.__username = filename
-        with open(filename, 'r') as file:
-            data = json.load(file)
-            for key , value in data.items():  # Fixed the iteration over items
-                setattr(self, key, value)
+    
+    def register(self):
+            self.__username = input("Enter username: ")
+            self.__email_address = input("Enter email address: ")
+            self.__password = input("Enter password: ")
 
-def register(self):
-        self.__username = input("Enter username: ")
-        self.__email_address = input("Enter email address: ")
-        self.__password = input("Enter password: ")
+            if self.check_existing_username(self.__username):
+                error_messages = [["Error", "Username already exists."]]
+                print_message(f"{error_messages[0][0]}: {error_messages[0][1]}", color="red")
+            else:
+                with open("Data\\Acounts_Data\\users.txt", "a") as file:
+                    file.write(f"{self.__username},{self.__email_address}\n")
+                print_message("Account successfully created.", color="green")
 
-        if self.check_existing_username(self.__username):
-            error_messages = [["Error", "Username already exists."]]
-            print_message(f"{error_messages[0][0]}: {error_messages[0][1]}", color="red")
-        else:
-            with open("Data\\Acounts_Data\\users.txt", "a") as file:
-                file.write(f"{self.__username},{self.__email_address}\n")
-            print_message("Account successfully created.", color="green")
+    def check_existing_username(self, username):
+            if os.path.exists("Data\\Acounts_Data\\users.txt"):
+                with open("Data\\Acounts_Data\\users.txt", "r") as file:
+                    for line in file:
+                        stored_username, _ = line.strip().split(',')
+                        if username == stored_username:
+                            return True
+            return False
+    def change_email(self):
+           new_email = input("Enter the new email address: ")
+           if not self.check_existing_username(self.__username):
+               with open("Data\\Acounts_Data\\users.txt", "r") as file:
+                   lines = file.readlines()
+               with open("Data\\Acounts_Data\\users.txt", "w") as file:
+                  for line in lines:
+                       stored_username, stored_email = line.strip().split(',')
+                       if stored_username == self.__username:
+                           file.write(f"{self.__username},{new_email}\n")
+                       else:
+                           file.write(line)
+               print_message("Email address updated successfully.", color="green")
+           else:
+               print_message("Username already exists.", color="red")
 
-def check_existing_username(self, username):
-        if os.path.exists("Data\\Acounts_Data\\users.txt"):
-            with open("Data\\Acounts_Data\\users.txt", "r") as file:
-                for line in file:
-                    stored_username, _ = line.strip().split(',')
-                    if username == stored_username:
-                        return True
-        return False
-# change email is in the user part
-# def change_email(self):
-#        new_email = input("Enter the new email address: ")
-#        if not self.check_existing_username(self.__username):
-#            with open("Data\\Acounts_Data\\users.txt", "r") as file:
-#                lines = file.readlines()
-#            with open("Data\\Acounts_Data\\users.txt", "w") as file:
-#                for line in lines:
-#                    stored_username, stored_email = line.strip().split(',')
-#                    if stored_username == self.__username:
-#                        file.write(f"{self.__username},{new_email}\n")
-#                    else:
-#                        file.write(line)
-#            print_message("Email address updated successfully.", color="green")
-#        else:
-#            print_message("Username already exists.", color="red")
+    def register_section(self):
+            options = ["Register", "Change Email"]
+            choice = globals.get_arrow_key_input(options)
 
-def register_section(self):
-        options = ["Register", "Change Email"]
-        choice = globals.get_arrow_key_input(options)
-
-        if choice == "Register":
-            self.register()
-#        elif choice == "Change Email":
-#           self.change_email()
+            if choice == "Register":
+                self.register()
+    #        elif choice == "Change Email":
+    #           self.change_email()
 
 
 class User:
-    def __init__(self, account, leading_projects, contributing_projects, is_active = True):
+    def __init__(self, username , email_address , password , leading_projects, contributing_projects):
+        account = Account(username=username, email_address=email_address, password=password)
         self.__account = account
         self.__leading_projects = leading_projects
         self.__contributing_projects = contributing_projects
-        self.__is_active = is_active
+    
+    def __del__(self) :
+        del self.__account
+        self.__leading_projects = None
+        self.__contributing_projects = None
     
     def display_projects(self):
         #opens the files of both types of projects the user has and then show the details somehow
@@ -139,3 +151,35 @@ class User:
         #needs a menu and checking if a project with the same id has been made before
         #and then make a file and other stuff
         pass
+    
+    def delete_project() :
+        #delete project from file of projecta , leader and all the members file
+        pass
+
+    def user_menu(self) :
+        options = ["Display Projects", "Add Project" , "Choose Project" , "Delete Project" , "Account Setting"]
+
+        while True:
+            choice =globals.get_arrow_key_input(options)
+            match choice :
+
+                case "Display Projects" :
+                    self.display_projects()
+
+                case "Add Project" :
+                    self.create_project()
+                
+                case "Choose Project" :
+                    self.choose_project()
+                
+                case "Delete Project" :
+                    self.delete_project()
+
+                case "Account Setting" :
+                    selected_option =  self.__account.account_setting()
+                    if selected_option == "sign out" :
+                        del self
+                        return 
+                    elif selected_option == "back" :
+                        continue
+
