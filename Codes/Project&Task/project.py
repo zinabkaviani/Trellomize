772c7a4edc0e97145080e742009b02c8fdb9006a
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-05-15 13:11:25 +0330

    project class base added

---
+++ Codes/Project&Task/project.py
@@ -0,0 +1,31 @@
+class project :
+    def __init__(self, id , title ,members ,leader , tasks ):
+        self.__id = id
+        self.__title = title
+        self.__members = members
+        self.__leader = leader 
+        self.__tasks = tasks
+
+    def add_member(self) :
+        """leaderr can add members via username"""
+        pass
+
+    def remove_members(self) :
+        """leader can remove members by chosing between members list via arrow key"""
+        pass
+
+    def dispaly_tasks(self) :
+        """display tasks of the project"""
+        pass
+
+    def add_tasks(self) :
+        """leader can add tasks and allocates them to members """
+        pass
+
+    def remove_tasks(self) :
+        """leader can remove tasks """ 
+        pass
+    def choose_task(self) :
+        """ chose tasks to open the task"""
+        pass
+
