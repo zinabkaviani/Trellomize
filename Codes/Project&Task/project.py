9c9c1bfa0f2aff39da15d4738679b1b930957cb6
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-05-19 19:09:14 +0330

    project complete except display tasks

--- Codes/Project&Task/project.py
+++ Codes/Project&Task/project.py
@@ -1,10 +1,10 @@
 from .. import globals
+import task
 
 class project :
-    def __init__(self, id , title,signed_in_username ,members ,leader , tasks ):
+    def __init__(self, id , title ,members ,leader , tasks ):
         self.__id = id
         self.__title = title
-        self.__signed_in_username = signed_in_username
         self.__members = members
         self.__leader = leader 
         self.__tasks = tasks
@@ -37,8 +37,8 @@ class project :
 
     def remove_members(self) :
         """leader can remove members by chosing between members list via arrow key"""
-        
-        choice = globals.get_arrow_key_input(self.__members,self.__members)
+        indices_list = list(range(len(self.__members)))
+        choice = self.__members[globals.get_arrow_key_input(self.__members,indices_list)]
         self.__members.remove(choice)
 
         self.__update_file_attributes("members")
@@ -72,40 +72,99 @@ class project :
                 "start_date" : start_time,
                 "end_date" : end_time,
                 "assignees" : [],
-                # "priority" : 
-                # "status" :
-                "history" : "",
+                "priority" : task.Priority.LOW,
+                "status" : task.Status.BACKLOG,
+                # "history" : "" ,
                 "comments" : []
             }
             globals.json.dump(data,file)
-            
-        
-        pass
 
     def remove_tasks(self) :
         """leader can remove tasks """ 
-        choice = globals.get_arrow_key_input(self.__tasks , self.__tasks)
+        indices_list = list(range(len(self.__tasks)))
+        choice = self.__tasks[globals.get_arrow_key_input(self.__tasks , indices_list)]
         self.__tasks.remove(choice)
         self.__update_file_attributes("tasks")
 
     def choose_task(self) :
         """choose tasks to open the task"""
-        choice = globals.get_arrow_key_input(self.__tasks,self.__tasks)
-        pass
+
+        available_tasks =[]
+        
+        for task_id in self.__tasks:
+            with open(f"Data\\Projects_Data\\{self.__id}\\Project_Tasks\\{task_id}.json" , "r") as file :
+                data= globals.json.load(file)
+                available_tasks.append(data["title"] + 10 * ' ' + task_id + '\n'  + data["description"])
+        available_tasks.append("Back")
+        available_indices = list(range(len(available_tasks)))
+        choice = self.__tasks[globals.get_arrow_key_input(options=available_tasks,available_indices= available_indices)]         
+        
+        with open(f"Data\\Projects_Data\\{self.__id}\\Project_Tasks\\{choice}.json" , "r") as file :
+            data= globals.json.load(file)
+            candidates_for_assignment = data["candidates_for_assignment"]
+            title = data["title"],
+            description= data["description"],
+            start_time = data["start_date"] ,
+            end_time = data["end_date"] ,
+            assigness = data["assignees"] ,
+            priority = data["priority"] , 
+            status = data["status"] ,
+            history = data["history"],
+            comments = data["comments"]
+            task_comments = [task.Comment(comment["account"],comment["text"],comment["date_of_comment"]) for comment in comments]
+            
+            task.Task(title=title,candidates_for_assignment=candidates_for_assignment,random_id = choice, description=description ,\
+                       start_date= start_time , end_date=end_time ,assignees= assigness ,\
+                      priority=priority, status= status ,history= history,comments=task_comments )
+
+
     def leave_project(self) :
         """users and leader can leave project"""
-        pass
+        print("do you sure about this?")
+        answer = input("yes/no")
+        if answer == "yes":
+            self.leaving_projects()    
+        else:
+            return
+        
+    def leaving_projects(self):
+        """helping function for leaving the project"""
+        if self.__leader == globals.signed_in_username:
+            for member in self.__members:
+                with open(f"Data\\Acounts_Data\\Users\\{member}.json" , "r") as file :
+                    data = globals.json.load(file)
+                    data["contributing_projects"].remove(self.__id)
+                    with open(f"Data\\Acounts_Data\\Users\\{member}.json" , "w") as updated_file :
+                        globals.json.dump(data,updated_file)
+            globals.os.remove(f"Data\\Projects_Data\\{self.__id}")
+
+        elif globals.signed_in_username in self.__members :
+            with open(f"Data\\Acounts_Data\\Users\\{globals.signed_in_username}.json" , "r") as file :
+                    data = globals.json.load(file)
+                    data["contributing_projects"].remove(self.__id)
+                    with open(f"Data\\Acounts_Data\\Users\\{globals.signed_in_username}.json" , "w") as updated_file :
+                        globals.json.dump(data,updated_file)
+            for task_id in self.__tasks :
+                with open(f"Data\\Projects_Data\\{self.__id}\\Project_Tasks\\{task_id}.json" , "r") as file :
+                    data= globals.json.load(file)
+                    data["assignees"].remove(globals.signed_in_username)
+                    with open(f"Data\\Projects_Data\\{self.__id}\\Project_Tasks\\{task_id}.json" , "w") as updated_file :
+                        globals.json.dump(data,updated_file)
+
+            self.__members.remove(globals.signed_in_username)
+            self.__update_file_attributes("members")
 
     def project_menu(self) :
         while True:
             self.dispaly_tasks() 
             options = ["Add Member" ,"Remove Member" ,"Add_Task" , "Remove Task" ,"Choose Task" ,"Leave Project" , "Exit Project"]
+            indices_list = list(range(len(options)))
             choice = None
-            if self.__leader == self.__signed_in_username :
-                choice = globals.get_arrow_key_input(options ,options )
+            if self.__leader == globals.signed_in_username :
+                choice = options[globals.get_arrow_key_input(options ,indices_list )]
             else :
-                available_options = ["Choose Task" , "Leave Project"] 
-                choice = globals.get_arrow_key_input(options ,available_options )
+                available_indices = [4, 5] 
+                choice = options[globals.get_arrow_key_input(options ,available_indices )]
 
             match choice :
                 case "Add Member" :
