0f17347db71f24e4eee7a1f42081b48dc212a5ce
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2024-05-22 01:24:30 +0330

    register updated

--- Codes/register.py
+++ Codes/register.py
@@ -1,6 +1,8 @@
 import bcrypt
 import globals
 import User.user as user
+import re
+import os
 
 def encode_password(password_input):
         return bcrypt.hashpw(password_input.encode('utf-8'), bcrypt.gensalt())
@@ -8,6 +10,27 @@ def encode_password(password_input):
 def check_password(entered_password , hashed_password):
         return bcrypt.checkpw(entered_password.encode('utf-8'), hashed_password)
 
+def check_email_format(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@gmail\.com$'
+    return re.match(pattern, email) is not None
+
+def check_existing_username(username):
+            if os.path.exists("Data\\Acounts_Data\\users.txt"):
+                with open("Data\\Acounts_Data\\users.txt", "r") as file:
+                    for line in file:
+                        stored_username, _ = line.strip().split(',')
+                        if username == stored_username:
+                            return True
+            return False
+    
+def check_existing_email(email_address):
+            if os.path.exists("Data\\Acounts_Data\\users.txt"):
+                with open("Data\\Acounts_Data\\users.txt", "r") as file:
+                    for line in file:
+                        _ , sorted_email_address = line.strip().split(',')
+                        if email_address == sorted_email_address:
+                            return True
+            return False
 
 def register():
     username =None
@@ -15,26 +38,31 @@ def register():
     password = None
     while True:
         print("pree Esc to exit")
+
         print("Enter username: ")
         username = globals.get_input_with_cancel()
         if username == None:
             return
+        
         print("Enter email address(correct format : name@gmail.com): ")
         email_address = globals.get_input_with_cancel()
         if email_address == None :
             return
+        
         print("Enter password: ")
         password = globals.get_input_with_cancel()
+        if password == None :
+            return
+        
+        if check_email_format( email_address) :
 
-        if globals.check_email_format( email_address) :
-
-            if globals.check_existing_username(username):
-                error_messages = [["Error", "Username already exists."]]
-                globals.print_message(f"{error_messages[0][0]}: {error_messages[0][1]}", color="red")
+            if check_existing_username(username):
+                error_messages = ["Error", "Username already exists."]
+                globals.print_message(f"{error_messages[0]}: {error_messages[1]}", color="red")
             
-            elif globals.check_existing_email(email_address) :
-                error_messages =[["Error" , "Email address already exists."]]
-                globals.print_message(f"{error_messages[0][0]}: {error_messages[0][1]}" , color ="red")
+            elif check_existing_email(email_address) :
+                error_messages =["Error" , "Email address already exists."]
+                globals.print_message(f"{error_messages[0]}: {error_messages[1]}" , color ="red")
             
             else:
                 with open("Data\\Acounts_Data\\users.txt", "a") as file:
@@ -43,17 +71,55 @@ def register():
                 password = encode_password(password)
                 break
         else :
-            error_messages =[["Error" , "Email format is incorrect."]]
-            globals.print_message(f"{error_messages[0][0]}: {error_messages[0][1]}" , color ="red")
+            error_messages =["Error" , "Email format is incorrect."]
+            globals.print_message(f"{error_messages[0]}: {error_messages[1]}" , color ="red")
 
+    
     data = {
         "account" :{"username":username ,"email_address":email_address ,"password":password},
         "leading_projects" : [],
         "contributing_projects" : []
     }
+    
     with open(f"Data\\Acounts_Data\\Users\\{username}.json","w" ) as file:
         globals.json.dump(data ,file)
     
+    
     return user.User(account=user.Account(username=username , email_address = email_address ,password=password),\
                      contributing_projects=[],leading_projects=[])
+
+def Log_in():
+
+    name, password = globals.get_input_with_cancel("Enter username or email address(email address correct format : name@gmail.com): "),\
+          globals.get_input_with_cancel("Enter password: ")
+    user_data = None
+    with open('users.txt', 'r') as file:
+        for line in file:
+            username , email_address = line.strip().split(',')        
+
+            if "@gmail.com" in name:
+                    if  name == email_address:
+                        with open(f"Data\\Acounts_Data\\Users\\{username}.json", 'r') as file:
+                            user_data = globals.json.load(file)
+                                
+                        if check_password(entered_password=password,hashed_password=user_data["password"]):
+                            return user.User(account=user.Account(username=username , email_address =  user_data["email_address"],\
+                                                                            password=encode_password(password)),contributing_projects=[],leading_projects=[])
+                        else:
+                            error_messages =["Error" , "Email address or Password is incorrect"]
+                            globals.print_message(f"{error_messages[0]}: {error_messages[1]}", color="red")        
+            else:
+                if name == username:
+                                
+                    with open(f"Data\\Acounts_Data\\Users\\{username}.json", 'r') as file:
+                        user_data = globals.json.load(file)
+                                
+                    if check_password(entered_password=password,hashed_password=user_data["password"]):
+                        return user.User(account=user.Account(username=username , email_address =  user_data["email_address"],\
+                                                                        password=encode_password(password)),contributing_projects=[],leading_projects=[])
+                    else:
+                        error_messages =["Error" , "Username or Password is incorrect"]
+                        globals.print_message(f"{error_messages[0]}: {error_messages[1]}", color="red")
+                
+  
 
